6a00e7c616f5805ca005ef8f29f4154936760718
PC <PC@HENGO>
2013-07-14 22:59:51 +0700

    finish #3 #4 not yet finish  50%

--- trautre_v1/app/Controller/MediasController.php
+++ trautre_v1/app/Controller/MediasController.php
@@ -19,46 +19,48 @@ class MediasController extends AppController {
 	}
         function upload() {                     
             if (empty($this->data)) {
+                    die("1. Upload Error!");
                     $this->render();
             } 
             else {      
                 $time = microtime ( 1 ) * 1000;
                 //$destination = realpath('../../app/webroot/img/uploads/') . '/';                                        
-                $dir = realpath('../../app/webroot/img/uploads/') . '/';                                        
+                $dir = realpath(WWW_ROOT . 'uploaded/images/data/') . '/';                                        
                 $file = $this->data['Media']['photos'];                    
                 $sizes = getimagesize($file['tmp_name']);                
                 $width = $sizes[0];$height = $sizes[1];               
                 $file['name']=md5($time).$file['name'];
 
-            if(@move_uploaded_file($file['tmp_name'],$dir.$file['name']))
-            {
-                //Dòng này là ảnh sẽ được upload
-                $main_img         = $dir.$file['name'];
+                if(@move_uploaded_file($file['tmp_name'],$dir.$file['name']))
+                {
+                    //Dòng này là ảnh sẽ được upload
+                    $main_img         = $dir.$file['name'];
 
-                //Dòng này là ảnh sẽ đóng dấu lên ảnh được upload
-                $watermark_img    = "tamps.png"; // use GIF or PNG, JPEG has no tranparency support
-                $padding         = 3; // distance to border in pixels for watermark image
-                $opacity        = 100;    // image opacity for transparent watermark
+                    //Dòng này là ảnh sẽ đóng dấu lên ảnh được upload
+                    $watermark_img    = "test.png"; // use GIF or PNG, JPEG has no tranparency support
+                    $padding         = 3; // distance to border in pixels for watermark image
+                    $opacity        = 100;    // image opacity for transparent watermark
 
-                $filetype = strtolower(substr($main_img,strlen($main_img)-4,4));
-                if($filetype == ".gif") $image = @imagecreatefromgif($main_img); 
-                if($filetype == ".jpg") $image = @imagecreatefromjpeg($main_img); 
-                if($filetype == ".png") $image = @imagecreatefrompng($main_img); 
-                $watermark     = imagecreatefrompng($dir.$watermark_img); // create watermark            
-                //$image         = imagecreatefromjpeg($main_img); // create main graphic
-                if(!$image || !$watermark) die("Error: main image or watermark could not be loaded!");
+                    $filetype = strtolower(substr($main_img,strlen($main_img)-4,4));
+                    if($filetype == ".gif") $image = @imagecreatefromgif($main_img); 
+                    if($filetype == ".jpg") $image = @imagecreatefromjpeg($main_img); 
+                    if($filetype == ".png") $image = @imagecreatefrompng($main_img); 
+                    $watermark     = imagecreatefrompng($dir.$watermark_img); // create watermark   
 
-            $watermark_size     = getimagesize($dir.$watermark_img);
-            $watermark_width     = $watermark_size[0];
-            $watermark_height     = $watermark_size[1];
+                    //$image         = imagecreatefromjpeg($main_img); // create main graphic
+                    if(!$image || !$watermark) die("Error: main image or watermark could not be loaded!");
 
-            $image_size     = getimagesize($main_img);
-            $dest_x         = $image_size[0] - $watermark_width - $padding;
-            $dest_y         = $image_size[1] - $watermark_height - $padding;
+                    $watermark_size     = getimagesize($dir.$watermark_img);
+                    $watermark_width     = $watermark_size[0];
+                    $watermark_height     = $watermark_size[1];
 
-            // copy watermark on main image
-            imagecopymerge($image, $watermark, $dest_x, $dest_y, 0, 0, $watermark_width, $watermark_height, $opacity);
-
+                    $image_size     = getimagesize($main_img);
+                    $dest_x         = $image_size[0] - $watermark_width - $padding;
+                    $dest_y         = $image_size[1] - $watermark_height - $padding;
+                // copy watermark on main image
+                imagecopymerge($image, $watermark, 0,0, 0, 0, 
+                        $watermark_width, $watermark_height, $opacity);
+                
                 
                 /*
                     // đường dẫn tới thu mục upload file ảnh
@@ -100,7 +102,9 @@ class MediasController extends AppController {
                  
                  */                          
                     //$media_type_id = 1;
-                    //$stat_id = 1;          
+                    //$stat_id = 1;   
+            
+            //save to database
                     $name = $_POST['Caption']; //nvarchar                     
                     if($_POST['Source'] == "")  $link = 'Tu lam';
                     else $link = $_POST['Source'];//nvarchar
@@ -121,15 +125,18 @@ class MediasController extends AppController {
                     );
                     
                     if ($this->Media->save($data)) {
-                            $this->Session->setFlash('Image has been added.');
+                            die("Upload success!!");
+                            //$this->Session->setFlash('Image has been added.');
                             //$this->redirect('/medias/v1');
                     } else {
+                            die("3. Upload Error!!");
                             //$this->Session->setFlash('Please correct errors below.');
                             //unlink($destination.$this->Upload->result);
                             $this->redirect('/medias/index');
-                    }    
-                    
-                    
+                    }                                            
+            }
+            else{
+                die("2. Upload Error!");
             }
             }
  
