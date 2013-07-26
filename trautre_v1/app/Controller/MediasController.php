0603a36f29b8209f17d0bcc0894fa81c6092a8a1
Ngô Thành Trung <trungvhit@gmail.com>
2013-07-26 09:12:01 +0700

    Update upload

--- trautre_v1/app/Controller/MediasController.php
+++ trautre_v1/app/Controller/MediasController.php
@@ -148,6 +148,43 @@ class MediasController extends AppController {
  
         }
     
+        
+    function upload_rename()
+    {
+        if(!empty($this->data))
+        {
+
+            $path = dirname(__DIR__)."/webroot/colin/";	
+            $valid_formats = array("jpg",  "bmp","jpeg");
+            $name = $this->data['Media']['photos']['name'];
+            if(strlen($name))
+            {
+                 list($txt, $ext) = explode(".", $name);
+                if(in_array($ext,$valid_formats)&& $this->data['Media']['photos']['size'] <= 1024*1024)
+                {
+                       // UPLOAD FILE LEN THU MUC TRUOC
+                      $upload_status = move_uploaded_file($this->data['Media']['photos']['tmp_name'], $path.$this->data['Media']['photos']['name']);
+                       if($upload_status){
+                         $view = new View($this);
+                          $html = $view->loadHelper('Upload');
+                         $new_name = $path.time().".jpg";
+                         // GOI HELPER THAY DOI ANH VOI NOI DUNG CHEN VAO
+                         //CHEN TEXT VAO HINH ANH
+                         /*if($html->watermark_text($path.$this->data['Media']['photos']['name'], $new_name))
+                          $demo_image = $new_name;
+                         }*/
+                         // GHI CHU VOI HINH ANH
+                         if($html->watermark_image($path.$this->data['Media']['photos']['name'], $new_name))
+                          $demo_image = $new_name;
+                         //LUU DU LIEU O DAY
+                         }
+                }
+             else
+
+                $msg="File size Max 256 KB or Invalid file format supports .jpg and .bmp";
+            }
+        }
+    }
     public function v1(){
             
         }
